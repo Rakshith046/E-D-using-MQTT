c346e7486b0e644b3fff76dbd0d7f39a86017397
Rakshith046 <91250557+Rakshith046@users.noreply.github.com>
2021-09-24 18:08:12 +0530

    Add files via upload

---
+++ project.py
@@ -0,0 +1,32 @@
+from random import randint, seed
+
+message= input("Enter the text:")
+variable = ""
+variable1 = ""
+#FOR ENCRYPTION:
+s=int(input("Set the SEED:"))
+seed(s)
+for i in message:
+    key= randint(1,10)
+    print(key,end=" ")
+    asc_val= ord(i)
+    xor_= asc_val ^ key
+    encrypted =chr(xor_)
+    variable += encrypted
+print()
+print("After Encrypting:",variable, end="")
+print()
+
+#FOR DECRYPTION:
+n= int(input("Enter the seed:"))
+seed(n)
+for i in variable:
+    key= randint(1,10)
+    print(key,end=" ")
+    asc = ord(i)
+    xor = asc ^ key
+    decrypt= chr(xor)
+    variable1 += decrypt
+print()
+print("After Decrypting:",variable1, end="")
+print()
